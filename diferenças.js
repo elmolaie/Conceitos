254f3df67b8ce3ce72f4a4e8ed20921b2826a244
elmolaie <elmolair@gmail.com>
2025-03-25 14:25:12 +0000

    Adcionar no comitt

---
+++ diferenças.js
@@ -0,0 +1,9 @@
+var nome= "Danilo" // variável
+
+var idade= 17 // variável
+
+const signo = "virgem" // contante
+
+// A diferença é que o com o var podemos declarar dois nomes diferentes na mesma variável, 
+// Com o let não é possível colocar dois nomes na mesma variável, então o valor é fixo
+// O const mantém o valor da variável constante
---
+++ funcoes.js
@@ -0,0 +1,3 @@
+function excutar (param){
+    return 0
+}
